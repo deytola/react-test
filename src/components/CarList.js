1fd968a0058e7e3da7eafba8f603d93f1ad09535
deytola <adebolaadetola65@gmail.com>
2020-05-04 19:21:36 +0100

    initial commit

---
+++ src/components/CarList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+class CarList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {cars: []}
+    }
+    
+    render(){
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+export default CarList;
